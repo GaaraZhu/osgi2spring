5a437cf035fb75bf9782c3283eb173375602b1f0
GZhu-tvnz <gary.zhu@tvnz.co.nz>
2019-04-03 14:40:56 +1300

    adds more checking rules

--- scanner.go
+++ scanner.go
@@ -53,9 +53,48 @@ func scanSingleFile(path string) (bool, error) {
 		return false, err
 	}
 
-	source := string(data)
-	if strings.Contains(source, "@Component") && !strings.Contains(source, "@org.springframework.stereotype.Component") {
+	source := string(data)                            // get the source code string
+	source = strings.Join(strings.Fields(source), "") // removes all spaces/taps/new lines
+
+	if !strings.Contains(source, "@Component") {
+		return false, nil // skip for non OSGIN components
+	}
+
+	//Rule No.1: spring component annotation should be present for OSGI components
+	if !strings.Contains(source, "@org.springframework.stereotype.Component") {
 		return true, nil
+	}
+
+	//Rule No.2: no-args activate method should be invoked after the construction
+	if strings.Contains(source, "voidactivate()") && !strings.Contains(source, "@PostConstruct") {
+		return true, nil
+	}
+
+	//Rule No.3: deactive method should be invoked before the object is destroyed
+	if strings.Contains(source, "@Deactivate") && !strings.Contains(source, "@PreDestroy") {
+		return true, nil
+	}
+
+	//Rule No.4 OSGI injected properties should be injected by spring
+	if strings.Contains(source, "@Reference") {
+		sr := strings.Split(path, "\\")
+		className := strings.Split(sr[len(sr)-1], ".java")[0]
+		sr = strings.Split(source, "@Reference")
+		for i, r := range sr {
+			if i == 0 {
+				continue
+			}
+			subsequentCode := strings.Split(r, ";")[0]
+			if !(strings.Contains(subsequentCode, "@Autowired") ||
+				strings.Contains(subsequentCode, "@Qualifier") ||
+				strings.Contains(subsequentCode, "@Resource") ||
+				strings.Contains(subsequentCode, "@Inject") ||
+				strings.Contains(source, "@Autowiredpublic"+className)) {
+				return true, nil
+			}
+		}
+
+		return false, nil
 	}
 
 	return false, nil
